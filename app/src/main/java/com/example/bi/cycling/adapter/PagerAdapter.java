ca828ee1e98a223db2e56e048161a2eb9f525358
walkermannu1 <24>
2016-08-28 19:33:24 +0800

    Add an adapter.

---
+++ app/src/main/java/com/example/bi/cycling/adapter/PagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.bi.cycling.adapter;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.bi.cycling.R;
+
+/**
+ * Created by guang on 2016/8/28.
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private static Activity activity;
+    private static final int[] IMAGES = {R.drawable.T1, R.drawable.T2, R.drawable.T3, R.drawable.T4, R.drawable.T5};
+    public static int sum = IMAGES.length;
+
+    public PagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+}
