319c4d2a2fadc546b6de98d69088019a1077a89b
walkermannum1 <2469377884@qq.com>
2016-08-29 11:01:50 +0800

    Add an circle.

---
+++ app/src/main/java/com/example/bi/cycling/adapter/Circle.java
@@ -0,0 +1,60 @@
+package com.example.bi.cycling.adapter;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v4.view.PagerAdapter;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.text.AttributedCharacterIterator;
+
+/**
+ * Created by user on 2016/8/29.
+ */
+public class Circle extends View {
+
+    private Paint paint;
+    private int radius = 16;
+    private int count = PagerAdapter.sum;
+    private int choosePosition = 0;
+    private int gap = 70;
+
+    public void choose(int pos) {
+        choosePosition = pos;
+        this.invalidate();
+    }
+
+    public Circle(Context context) {
+        this(context, null);
+    }
+    public Circle(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    public Circle(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context,attrs,defStyleAttr);
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int widths = getWidth();
+        int height = getHeight();
+
+        int startPx = (widths - (count - 1) * gap) / 2;
+
+        for (int i = 0; i < count; i++) {
+            if (choosePosition == i) {
+                paint.setColor(Color.CYAN);
+                canvas.drawCircle(startPx + i * gap, height - 50, radius + 4, paint);
+            } else {
+                paint.setColor(Color.WHITE);
+                canvas.drawCircle(startPx + i * gap, height - 50, radius, paint);
+            }
+        }
+    }
+}
