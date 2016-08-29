356ecd185cf45309e967fbb08ad512578496c08a
walkermannum1 <2469377884@qq.com>
2016-08-29 13:44:31 +0800

    Complete Secondactivity

--- app/src/main/java/com/example/bi/cycling/SecondActivity.java
+++ app/src/main/java/com/example/bi/cycling/SecondActivity.java
@@ -1,9 +1,26 @@
 package com.example.bi.cycling;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 /**
  * Created by user on 2016/8/29.
  */
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("Welcome coming!");
+        textView.setTextSize(28);
+        textView.setTextColor(Color.BLUE);
+        textView.setGravity(Gravity.CENTER);
+
+        setContentView(textView);
+    }
 }
